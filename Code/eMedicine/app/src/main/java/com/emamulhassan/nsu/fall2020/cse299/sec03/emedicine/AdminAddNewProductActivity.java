f02abae90c96a6a565780f0dd0b667681208f80a
Emamul Hassan <emamul.hassan@northsouth.edu>
2020-12-28 22:32:58 +0600

    Enhancement #16: Initialized all ids of AdminAddNewProductActivity.xml
    Initialized Imageview, buttons and edittext in AdminAddNewProductActivity.java

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/AdminAddNewProductActivity.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/AdminAddNewProductActivity.java
@@ -2,11 +2,19 @@ package com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
     private String CategoryName;
+    private ImageView InputProductImage;
+    private EditText InputProductName, InputProductDescription, InputProductPrice;
+    private Button AddNewProductButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,13 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
         CategoryName = getIntent().getExtras().get("category").toString();
 
-        Toast.makeText(this,CategoryName, Toast.LENGTH_SHORT).show();
+        InputProductImage = (ImageView) findViewById(R.id.select_product_image);
+        InputProductName = (EditText) findViewById(R.id.product_name);
+        InputProductDescription = (EditText) findViewById(R.id.product_description);
+        InputProductPrice = (EditText) findViewById(R.id.product_price);
+        AddNewProductButton = (Button) findViewById(R.id.add_new_product);
+
+
+
     }
 }
