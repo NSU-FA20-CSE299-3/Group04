a408a61a123c104ebd11ace1ba7c49673e3a4fa1
Emamul Hassan <emamul.hassan@northsouth.edu>
2020-12-29 19:43:12 +0600

    Enhancement #16: Product Data for add new product actiivty
    Created ProductData function in add new product actiivty for admin to must input every single product details(image, name, description and price).

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/AdminAddNewProductActivity.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/AdminAddNewProductActivity.java
@@ -1,19 +1,27 @@
 package com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
-    private String CategoryName;
+    private String CategoryName, Pname, Description, Price;
     private ImageView InputProductImage;
     private EditText InputProductName, InputProductDescription, InputProductPrice;
     private Button AddNewProductButton;
+    private static final int GalleryPick = 1;
+    private Uri ImageUri;
+
 
 
     @Override
@@ -30,6 +38,69 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         AddNewProductButton = (Button) findViewById(R.id.add_new_product);
 
 
+        InputProductImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+            }
+        });
+
+        AddNewProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValidateProductData();
+            }
+        });
+
+
 
     }
+
+    private void OpenGallery()
+    {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GalleryPick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==GalleryPick  &&  resultCode==RESULT_OK  &&  data!= null)
+        {
+            ImageUri = data.getData();
+            InputProductImage.setImageURI(ImageUri);
+        }
+    }
+
+    private void ValidateProductData()
+    {
+        Description = InputProductDescription.getText().toString();
+        Price = InputProductPrice.getText().toString();
+        Pname = InputProductName.getText().toString();
+
+        if (ImageUri == null)
+        {
+            Toast.makeText(this, "Select Product Image!", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(Pname))
+        {
+            Toast.makeText(this, "Please Enter Product Product Name!", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(Description))
+        {
+            Toast.makeText(this, "Please Enter Product Details!", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(Price))
+        {
+            Toast.makeText(this, "Please Enter Product Price!", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+
+
 }
