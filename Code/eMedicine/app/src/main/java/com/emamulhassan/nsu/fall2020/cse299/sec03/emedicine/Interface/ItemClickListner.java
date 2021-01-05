186b8f70d1500c1942b5d3b4bbf81e5d0a323e0e
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-05 22:54:46 +0600

    Enhancement #18: Creating new Package and a model class
     Creating new Package named Interface and adding ItemClickListner.java and creating products.java in existing model class.

---
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/Interface/ItemClickListner.java
@@ -0,0 +1,8 @@
+package com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner
+{
+    void onClick(View view, int position, boolean isLongClick);
+}
