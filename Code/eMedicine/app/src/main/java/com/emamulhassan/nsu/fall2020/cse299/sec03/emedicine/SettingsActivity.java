9c94ee92837faefd98e9fe93dd8b328ceb965508
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-06 23:59:32 +0600

    Enhancement #19: Initializing all variables of settings activity.
    Initialized all variables of settings activity.

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/SettingsActivity.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/SettingsActivity.java
@@ -3,12 +3,33 @@ package com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SettingsActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingsActivity extends AppCompatActivity
+
+    private TextView closeTextBtn, saveTextButton;
+    private CircleImageView profileImageView;
+    private TextView profileChangeTextBtn;
+    private EditText fullNameEditText, userPhoneEditText, addressEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        closeTextBtn = (TextView) findViewById(R.id.close_settings_btn);
+        saveTextButton = (TextView) findViewById(R.id.update_account_settings_btn);
+        profileImageView = (CircleImageView) findViewById(R.id.settings_profile_image);
+        profileChangeTextBtn = (TextView) findViewById(R.id.profile_image_change_btn);
+        fullNameEditText = (EditText) findViewById(R.id.settings_full_name);
+        userPhoneEditText = (EditText) findViewById(R.id.settings_phone_number);
+        addressEditText = (EditText) findViewById(R.id.settings_address);
+
+
+        
     }
 }
