a6da2ee8b196622cc8d61989105fdafb2e733f39
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-09 18:14:30 +0600

    Enhancement #20: Initialized all id's of ProductDetailsActivity
    Initialized all id's of ProductDetailsActivity.xml

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/ProductDetailsActivity.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/ProductDetailsActivity.java
@@ -3,12 +3,30 @@ package com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ProductDetailsActivity extends AppCompatActivity {
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+
+public class ProductDetailsActivity extends AppCompatActivity
+{
+    private ImageView productImage;
+    private TextView productName, productDescription, productPrice;
+    private ElegantNumberButton numberButton;
+    private Button addToCartButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+        productImage = (ImageView) findViewById(R.id.product_image_details);
+        productName = (TextView) findViewById(R.id.product_name_details);
+        productDescription = (TextView) findViewById(R.id.product_description_details);
+        productPrice = (TextView) findViewById(R.id.product_price_details);
+        numberButton = (ElegantNumberButton) findViewById(R.id.number_btn);
+
     }
 }
