6566f36437c2932c13271baee89c28b79531c28a
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-11 17:21:41 +0600

    Enhancement #21: Created onStart Methods
    Created onStart Methods to retreive all data from firebase database.

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/CartActivity.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/CartActivity.java
@@ -8,6 +8,12 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine.Model.Cart;
+import com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine.Prevalent.Prevalent;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CartActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
@@ -30,6 +36,23 @@ public class CartActivity extends AppCompatActivity {
 
         NextProcessBtn = (Button) findViewById(R.id.next_btn);
         txtTotalAmount = (TextView) findViewById(R.id.total_price);
+
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        FirebaseRecyclerOptions<Cart> =
+        new FirebaseRecyclerOptions.Builder<Cart>()
+                .setQuery(cartListRef.child("User View")
+                        .child(Prevalent.currentOnlineUser.getPhone())
+                        .child("Products"), Cart.class)
+                .build();
 
 
     }
