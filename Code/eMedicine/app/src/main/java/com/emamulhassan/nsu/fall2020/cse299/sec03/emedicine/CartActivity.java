6b30ae9ebe8061cddd58362c9954a0b4c3f0a956
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-11 17:38:47 +0600

    Enhancement #21: Updated Methods to retreive all data from firebase database
    Updated Methods to retreive all data from firebase databas using FirebaseRecyclerAdapter.

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/CartActivity.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/CartActivity.java
@@ -1,15 +1,21 @@
 package com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine.Model.Cart;
 import com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine.Prevalent.Prevalent;
+import com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine.ViewHolder.CartViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -47,13 +53,33 @@ public class CartActivity extends AppCompatActivity {
 
         final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
 
-        FirebaseRecyclerOptions<Cart> =
+        FirebaseRecyclerOptions<Cart> options =
         new FirebaseRecyclerOptions.Builder<Cart>()
                 .setQuery(cartListRef.child("User View")
                         .child(Prevalent.currentOnlineUser.getPhone())
                         .child("Products"), Cart.class)
                 .build();
 
+        FirebaseRecyclerAdapter<Cart, CartViewHolder> adapter = new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull CartViewHolder holder, int position, @NonNull Cart model)
+            {
+                holder.txtProductQuantity.setText("Quantity = " + model.getQuantity());
+                holder.txtProductPrice.setText("Price " + model.getPrice() + "BDT");
+                holder.txtProductName.setText(model.getPname());
+            }
 
+            @NonNull
+            @Override
+            public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+            {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_items_layout, parent, false);
+                CartViewHolder holder = new CartViewHolder(view);
+                return holder;
+            }
+        };
+        
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
     }
 }
