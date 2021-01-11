a124ae122bf857c19eac5b9ee739155d9654f2f7
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-11 17:15:02 +0600

    Enhancement #21: Generated constructors of all string types
    Generated constructors of all string types of Cart.java model class.

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/Model/Cart.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/Model/Cart.java
@@ -1,4 +1,19 @@
 package com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine.Model;
 
-public class Cart {
+public class Cart
+{
+    private String pid, pname, price, quantity, discount;
+
+    public Cart()
+    {
+
+    }
+
+    public Cart(String pid, String pname, String price, String quantity, String discount) {
+        this.pid = pid;
+        this.pname = pname;
+        this.price = price;
+        this.quantity = quantity;
+        this.discount = discount;
+    }
 }
