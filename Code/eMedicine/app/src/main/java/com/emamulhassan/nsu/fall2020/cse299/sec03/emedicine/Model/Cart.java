be8d00883476f50d3b896072c74e1693c1b052b8
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-11 17:16:15 +0600

    Enhancement #21: Generated getter and setter for all string types
    Generated getter and setter for all string types of Cart.java model class.

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/Model/Cart.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/Model/Cart.java
@@ -16,4 +16,44 @@ public class Cart
         this.quantity = quantity;
         this.discount = discount;
     }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
 }
