4c48fd4544c08a16970d1d2f3fd9e8347370c991
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-12 17:00:45 +0600

    Enhancement #22: Initialized all ids of ConfirmFinalOrderActivity
    Initialized all ids of ConfirmFinalOrderActivity

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/ConfirmFinalOrderActivity.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/ConfirmFinalOrderActivity.java
@@ -3,12 +3,26 @@ package com.emamulhassan.nsu.fall2020.cse299.sec03.emedicine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ConfirmFinalOrderActivity extends AppCompatActivity {
+
+    private EditText nameEditText, phoneEditText, addressEditText, cityEditText;
+    private Button confirmOrderBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_final_order);
+
+        confirmOrderBtn = (Button) findViewById(R.id.confirm_final_order_btn);
+        nameEditText = (EditText) findViewById(R.id.shippment_name);
+        phoneEditText = (EditText) findViewById(R.id.shippment_phone_number);
+        addressEditText = (EditText) findViewById(R.id.shippment_address);
+        cityEditText = (EditText) findViewById(R.id.shippment_city);
+
+
     }
 }
