b7189540608b8216897b208e961436d6f68639f2
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-01-14 19:11:08 +0600

    Enhancement #15: Added setOnClickListener for AdminLogout button
    Added setOnClickListener for AdminLogout button to logout from admin activity and go back to Login Activity.

--- Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/AdminCategoryActivity.java
+++ Code/eMedicine/app/src/main/java/com/emamulhassan/nsu/fall2020/cse299/sec03/emedicine/AdminCategoryActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class AdminCategoryActivity extends AppCompatActivity {
     private ImageView medicine, babyCare;
     private ImageView feminineHygine, diabetesMedicine;
     private ImageView painRelief, others;
+    private Button AdminLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,16 @@ public class AdminCategoryActivity extends AppCompatActivity {
 
         painRelief = (ImageView) findViewById(R.id.m_pain);
         others = (ImageView) findViewById(R.id.m_others);
+
+        AdminLogout = (Button) findViewById(R.id.admin_logout);
+
+        AdminLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         medicine.setOnClickListener(new View.OnClickListener() {
